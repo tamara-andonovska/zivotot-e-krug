21e178e9548839f0f994478378a87c8cb7cdf4f1
Tamara <t.andonovska8@gmail.com>
2022-03-17 10:43:26 +0100

    design changes + datepicker

--- app/src/main/java/com/tamara/zivototekrug/OrganizerHomeActivity.java
+++ app/src/main/java/com/tamara/zivototekrug/OrganizerHomeActivity.java
@@ -1,22 +1,49 @@
 package com.tamara.zivototekrug;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class OrganizerHomeActivity extends AppCompatActivity {
 
     Button btnLogOut;
+    TextView home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_home);
+
+        home = findViewById(R.id.organizer_text_home);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user");
+        String uid = mUser.getUid();
+
+        ref.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                home.setText("Welcome, " + user.getFullName()+"!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
 
         btnLogOut = findViewById(R.id.logout_btn);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/tamara/zivototekrug/RequestActivity.java
+++ app/src/main/java/com/tamara/zivototekrug/RequestActivity.java
@@ -3,7 +3,10 @@ package com.tamara.zivototekrug;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -35,7 +38,8 @@ public class RequestActivity extends AppCompatActivity {
     private int secs;
 
     Calendar mCalendar;
-    DatePicker datePicker;
+    TextView date;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
     TimePicker timePicker;
 
     RadioGroup radioGroup1;
@@ -88,15 +92,32 @@ public class RequestActivity extends AppCompatActivity {
         mins = mCalendar.get(Calendar.MINUTE);
         secs = mCalendar.get(Calendar.SECOND);
 
-        datePicker = findViewById(R.id.date_picker);
-        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+        date = findViewById(R.id.date_picker);
+        date.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
-                RequestActivity.this.year = year;
-                RequestActivity.this.month = month;
-                RequestActivity.this.day = day;
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(RequestActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
         });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String d = day + "/" + month + "/" + year;
+                date.setText(d);
+            }
+        };
 
         timePicker = findViewById(R.id.time_picker);
         timePicker.setHour(this.hour);
@@ -138,8 +159,7 @@ public class RequestActivity extends AppCompatActivity {
             repeatedActivity = "yes";
         }
 
-        int m = datePicker.getMonth() + 1;
-        String dateString = datePicker.getDayOfMonth() + "-" + m + "-" + datePicker.getYear();
+        String dateString = date.getText().toString();
 
         String timeString = timePicker.getHour() + ":" + timePicker.getMinute();
 
